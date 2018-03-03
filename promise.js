9b6c7f577489a0578c4ef650ba1e7aa115f4f4c3
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2018-03-03 17:18:15 +0900

    feature:add promise server

---
+++ promise.js
@@ -0,0 +1,40 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'rkd123'
+});
+
+
+const express = require('express');
+const app = express();
+
+function query(q) {
+    return new Promise(function(resolve, reject) {
+        connection.query(q, function (error, results) {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(results);
+            }
+        });
+    });
+}
+
+//내부 에러시 제대로 캐치 되지 않음
+app.get('/', (req, res) => {
+    query('SELECT 1 + 1 AS solution')
+        .then((results1) => {
+            return query('SELECT 1 + 2 AS solution')
+                .then((results2) => {
+                    let solution1 = results1[0].solution;
+                    let solution2 = results2[0].solution;
+                    res.send('The solution is: ' + (solution1 + solution2));
+                })
+        })
+        .catch((err) => {
+            res.send(err);
+        })
+});
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
--- run_mysql_cont.sh
+++ run_mysql_cont.sh
@@ -4,4 +4,4 @@ if [[ "$(docker ps -a | grep some-mysql 2> /dev/null)" != "" ]]; then
   docker rm -f some-mysql
 fi
 
-docker run --name some-mysql -e MYSQL_ROOT_PASSWORD=rkd123 -d -p 3306:3306 mysql bash
+docker run --name some-mysql -e MYSQL_ROOT_PASSWORD=rkd123 -d -p 3306:3306 mysql
